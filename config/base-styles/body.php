7f4b7ff3c4a6c52e08aa5c637f15eb39a507ebb5
seothemes <seothemeswp@gmail.com>
2020-04-09 21:54:21 +0800

    Add body base styles

--- config/base-styles/body.php
+++ config/base-styles/body.php
@@ -1,3 +1,57 @@
 <?php
 
-return [];
+return [
+	[
+		'type'     => 'color',
+		'settings' => 'background_color',
+		'label'    => __( 'Background Color', 'mai-customizer' ),
+		'output'   => [
+			[
+				'element'  => 'body',
+				'property' => 'background-color',
+			],
+		],
+	],
+	[
+		'type'     => 'color',
+		'settings' => 'text_color',
+		'label'    => __( 'Text Color', 'mai-customizer' ),
+		'output'   => [
+			[
+				'element'  => 'body',
+				'property' => 'color',
+			],
+		],
+	],
+	[
+		'type' => 'divider',
+	],
+	[
+		'type'     => 'typography',
+		'settings' => 'typography',
+		'label'    => __( 'Typography', 'mai-customizer' ),
+		'default'  => [
+			'font-family'    => '',
+			'variant'        => '',
+			'font-size'      => '',
+			'line-height'    => '',
+			'letter-spacing' => '',
+			'text-transform' => '',
+		],
+		'output'   => [
+			[
+				'element' => 'body',
+			],
+			[
+				'element' => [
+					'body .editor-styles-wrapper',
+					'body .editor-styles-wrapper .editor-post-title__input',
+				],
+				'context' => [ 'editor' ],
+			],
+		],
+	],
+	[
+		'type' => 'divider',
+	],
+];
