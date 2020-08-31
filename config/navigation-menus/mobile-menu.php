6aa84f7965f648c074810e1535b5cd11f676b52f
seothemes <seothemeswp@gmail.com>
2020-08-31 21:35:48 +0800

    Changes

--- config/navigation-menus/mobile-menu.php
+++ config/navigation-menus/mobile-menu.php
@@ -1,12 +1,3 @@
 <?php
 
-return [
-	[
-		'type'              => 'text',
-		'label'             => __( 'Breakpoint', 'mai-engine' ),
-		'description'       => __( 'The largest screen width at which the mobile menu becomes active, in pixels.', 'mai-engine' ),
-		'settings'          => 'breakpoint',
-		'sanitize_callback' => 'absint',
-		'default'           => mai_get_breakpoint(),
-	],
-];
+return [];
